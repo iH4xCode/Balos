95467be733d5185a6217756aa9f974042c787efa
iH4xCode <ayakamitsu1772@gmail.com>
2024-10-12 21:33:03 +0800

    commit

---
+++ App/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import  HomepageView, AboutpageView
+
+
+urlpatterns = [
+    path('', HomepageView.as_view(), name = 'home'),
+    path('about/', AboutpageView.as_view(),name = 'about')
+]
